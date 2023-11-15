74736dfb7b0f186036a25c74617bebf015f4ad10
Sajib Khan <sajibkhan.mern@gmail.com>
2023-11-15 22:36:59 +0600

    signup responsive

--- src/pages/LoginReg.jsx
+++ src/pages/LoginReg.jsx
@@ -10,6 +10,7 @@ const LoginReg = () => {
   const auth = getAuth();
   let navigate=useNavigate()
   let [loader,setLoader]=useState(false)
+  let [condition,setCondition]=useState(false)
 
   let [email,setEmail]=useState("")
   let [name,setName]=useState("")
@@ -22,7 +23,7 @@ const LoginReg = () => {
   let handleSignup=()=>{
     setActive(true)
   }
-  let handleLogin=()=>{
+  let handleLogin=(sajib)=>{
     setActive(false)
   }
 
@@ -65,6 +66,7 @@ const LoginReg = () => {
     setPassword("")
     setActive(false)
     setLoader(false)
+    
   });
   })
   .catch((error) => {
@@ -258,16 +260,20 @@ const LoginReg = () => {
             </div>
 
         </div>
-
-
-       
-
     </div>
     </div>
      {/* For mobile */}
+     
+     
+     
+
+     
+     
+     {condition?
      <div className='md:hidden'>
+      {/* Sign up */}
      <div className='flex justify-center items-center flex-col h-screen bg-[#c9d6ff] px-4'>
-     <div className='bg-white flex justify-center items-center flex-col w-full rounded-lg px-6'>
+     <div className='bg-white flex justify-center items-center flex-col w-full rounded-lg px-6 sm:px-16'>
             <h1 className='text-center text-3xl text-black font-mon font-bold pb-7 pt-14'>Sign Up</h1>
             <div className='flex gap-x-4 pb-4'>
                 <div onClick={hangleGoogle1} className='border border-solid border-[#ccc] rounded-lg px-1.5 py-1.5'>
@@ -289,27 +295,61 @@ const LoginReg = () => {
             <input onChange={(e)=>setPassword(e.target.value)} value={password} className='w-full bg-[#EEEEEE] py-1.5 px-5 my-1.5 rounded placeholder:text-sm ' type="password" placeholder='Password'/>
             {/* <p className='text-black text-sm font-medium font-mon pt-3 pb-5'>Forget Your Password?</p> */}
             {loader?
-            <button>
-              <Bars
-                height="30"
-                width="80"
-                color="blue"
-                ariaLabel="bars-loading"
-                wrapperStyle={{
-                marginTop:"15px",
-                }}
-                wrapperClass=""
-                visible={true}
-              />
+            <button><Bars height="27"  width="80" color="blue" ariaLabel="bars-loading" wrapperStyle={{marginTop:"15px",marginBottom:"20px",}} wrapperClass="" visible={true}/>
             </button>
             :
-            <button onClick={handleSignupForm}  className='bg-[#512DA7] py-2 px-8 text-white text-base font-mon font-semibold rounded-md mb-5 mt-5'>Sign Up</button>
+            <button onClick={handleSignupForm}  className='bg-[#512DA7] py-2 px-8 text-white text-base font-mon font-semibold rounded-md mb-6 mt-5'>Sign Up</button>
             }
-            <p className='mb-12 text-xs text-black font-mon font-normal'>Already have an account ? <span className='text-red-700 cursor-pointer text-xs font-semibold'>Sign In</span></p>
+            <p className='mb-12 text-xs text-black font-mon font-medium'>Already have an account ? <span onClick={()=>setCondition(false)} className='text-[#EA6C4B] cursor-pointer text-xs font-semibold'>Sign In</span></p>
             
             </div>
       </div>
      </div>
+     :
+     <div className='md:hidden'>
+      {/* Sing in */}
+     <div className='flex justify-center items-center flex-col h-screen bg-[#c9d6ff] px-4'>
+      <div className='bg-white flex justify-center items-center flex-col w-full rounded-lg px-6 sm:px-16'>
+      <h1 className='text-center text-3xl text-black font-mon font-bold pb-7 pt-14'>Sign In</h1>
+            <div className='flex gap-x-4 pb-4'>
+                <div className='border border-solid border-[#ccc] rounded-lg px-1.5 py-1.5'>
+                <FaGooglePlusG onClick={handleGoogle2} className='text-lg'/>
+                </div>
+                <div className='border border-solid border-[#ccc] rounded-lg px-2 py-1.5'>
+                <FaFacebookF className='text-sm'/>
+                </div>
+                <div className='border border-solid border-[#ccc] rounded-lg px-2 py-1.5'>
+                <FaGithub className='text-sm'/>
+                </div>
+                <div className='border border-solid border-[#ccc] rounded-lg px-2 py-1.5'>
+                <FaLinkedinIn className='text-sm'/>
+                </div>
+            </div>
+            
+            <p className='text-black text-sm font-normal font-mon pb-4'>or use your email password</p>
+            <input onChange={(e)=>setEmail2(e.target.value)} value={email2} className='w-full bg-[#EEEEEE] py-1.5 px-5 my-1.5 rounded placeholder:text-sm ' type="text" placeholder='Email'/>
+            <input onChange={(e)=>setPassword2(e.target.value)} value={password2} className='w-full bg-[#EEEEEE] py-1.5 px-5 my-1.5 rounded placeholder:text-sm ' type="password" placeholder='Password'/>
+            <p className='text-black text-sm font-medium font-mon pt-5 pb-5 '>Forget Your Password?</p>
+            {loader?
+            <button>
+            <Bars
+              height="27"
+              width="80"
+              color="blue"
+              ariaLabel="bars-loading"
+              wrapperStyle={{marginBottom:"20px"}}
+              wrapperClass=""
+              visible={true}
+            />
+          </button>
+            :
+            <button onClick={handleSignInForm} className='bg-[#512DA7] py-2 px-8 text-white text-base font-mon font-semibold rounded-md mb-6 mt-1'>Sign In</button>
+            }
+            <p className='mb-12 text-xs text-black font-mon font-medium'>Don’t have an account ? <span onClick={()=>setCondition(true)} className='text-[#EA6C4B] cursor-pointer text-xs font-semibold'>Sign up</span></p>
+      </div>
+      </div>
+     </div>
+     }
     </>
   )
 }
